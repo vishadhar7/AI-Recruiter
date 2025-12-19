01d1936de8df824b5c59f3e54e5e1e6925eb212c
vishadhar7 <vishadharreddy0722@gmail.com>
2025-12-19 21:01:14 +0530

    Added static frontend hosting

--- front_end/first_page/first_page.js
+++ front_end/first_page/first_page.js
@@ -1,3 +1,6 @@
 function goToCandidateScreening() {
   window.location.href = "../second_page/second_page.html";
 }
+function goToInterviewAI() {
+  window.location.href = "../fourth_page/fourth_page.html";
+}
